f63d577148a24f6ab913179c32cf06a637fc80c9
Kudzam <kuda.zambuko@yahoo.co.uk>
2026-02-03 16:52:09 +0000

    These are connection for the database

---
+++ ProductsAPI/Data/ProductsDbContext.cs
@@ -0,0 +1,11 @@
+﻿using Microsoft.EntityFrameworkCore;
+using ProductsAPI.Models;
+
+namespace ProductsAPI.Data
+{
+    public class ProductsDbContext(DbContextOptions<ProductsDbContext> options): DbContext(options)
+    {
+        //adding a database set resulting in the creation of our table
+        public DbSet<ProductsModel> ProductsModels { get; set; }
+    }
+}
