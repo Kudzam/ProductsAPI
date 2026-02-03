7725eeb0fb7f94d3c9ef7a1fddf7cd8e53404c4c
Kudzam <kuda.zambuko@yahoo.co.uk>
2026-02-03 17:34:48 +0000

    Data Migrated into the database

--- ProductsAPI/Migrations/20260203173111_Seeding.Designer.cs
+++ ProductsAPI/Migrations/20260203173111_Seeding.Designer.cs
@@ -2,6 +2,7 @@
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
 using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 using ProductsAPI.Data;
 
@@ -10,9 +11,11 @@ using ProductsAPI.Data;
 namespace ProductsAPI.Migrations
 {
     [DbContext(typeof(ProductsDbContext))]
-    partial class ProductsDbContextModelSnapshot : ModelSnapshot
+    [Migration("20260203173111_Seeding")]
+    partial class Seeding
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        /// <inheritdoc />
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
---
+++ ProductsAPI/Migrations/20260203173111_Seeding.cs
@@ -0,0 +1,45 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+#pragma warning disable CA1814 // Prefer jagged arrays over multidimensional
+
+namespace ProductsAPI.Migrations
+{
+    /// <inheritdoc />
+    public partial class Seeding : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.InsertData(
+                table: "ProductsModels",
+                columns: new[] { "Id", "Contractor", "ProductLabel", "ProductType", "Quantity" },
+                values: new object[,]
+                {
+                    { 1, "Outsource", "Levi", "Jeans", 90 },
+                    { 2, "Outsource", "Next", "T-Shirt", 38 },
+                    { 3, "InHouse", "RiverIsland", "Jacket", 50 }
+                });
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DeleteData(
+                table: "ProductsModels",
+                keyColumn: "Id",
+                keyValue: 1);
+
+            migrationBuilder.DeleteData(
+                table: "ProductsModels",
+                keyColumn: "Id",
+                keyValue: 2);
+
+            migrationBuilder.DeleteData(
+                table: "ProductsModels",
+                keyColumn: "Id",
+                keyValue: 3);
+        }
+    }
+}
